f635c5c663b958b85e291096e7286b90fcac6293
asaloyomonboyeva23-rgb <asaloyomonboyeva23@gmail.com>
2025-10-08 12:58:35 +0500

    Add files via upload

---
+++ asaloy1.py
@@ -0,0 +1,16 @@
+
+celsius = float(input(" enter the temperature :"))
+fahreheit = (celsius * 9/5 )+ 32 
+kelvin = celsius + 273.15 
+print(f" celsius temperature :{ celsius:.2f} ")
+print(f" fahrenheit temperature : {fahreheit:.2f}")
+print(f" kelvin temperature : {kelvin:.2f} ")
+
+2
+r = float(input("enter the radius : "))
+pi = 3.14159
+Area = pi * r**2
+Circumference = 2 * pi * r
+print(f" the area  :{ Area:.2f} ")
+print(f" the circumference :{ Circumference:.2f} ")
+3 
