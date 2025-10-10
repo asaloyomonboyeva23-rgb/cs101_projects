0043fab2ed840da787ecd664ea31086ab1225469
asaloyomonboyeva23-rgb <asaloyomonboyeva23@gmail.com>
2025-10-10 09:54:25 +0500

    Update asaloy1.py

--- asaloy1.py
+++ asaloy1.py
@@ -14,3 +14,13 @@ Circumference = 2 * pi * r
 print(f" the area  :{ Area:.2f} ")
 print(f" the circumference :{ Circumference:.2f} ")
 3 
+
+student_name = input("Enter your name  : ")
+gpa = float(input(" enter your gpa :"))
+credit_hours =int(input("enter tour creadit hours :"))
+deans_list = gpa >= 3.5 and credit_hours >=12
+
+print(f"student information : {student_name} , {gpa} , {credit_hours}")
+print(f" Deans_status : {"true" if deans_list else " False"}")
+print(f" how many gpa status needed :{"no need" if gpa >= 3.5 else 3.5 - gpa}")
+print(f" how many more credits needed : {"no need" if credit_hours >= 12 else 12 - credit_hours}")
